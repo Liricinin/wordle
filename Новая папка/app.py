610a20047e41a3f59933396ee0851d72e19bcb69
Liricinin <fanshu090909@gmail.com>
2026-01-30 23:09:05 +0300

    Update app.py

--- Новая папка/app.py
+++ Новая папка/app.py
@@ -1,3 +1,17 @@
-@app.route("/wordle")
-def wordle():
-    return render_template("wordle.html")
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+WORDS = [
+    "арбуз", "домик", "маши", "книга", "ручка",
+    "школа", "окноо", "рекаа", "мирок", "котик"
+]
+
+@app.route("/")
+def index():
+    word = random.choice(WORDS)
+    return render_template("index.html", word=word)
+
+if name == "__main__":
+    app.run()
